eb6369e085777051087b4c8af72356f917d62ecf
christianlewis091 <christian.lewis091@gmail.com>
2022-08-29 09:55:53 +1200

    first commit

---
+++ ox_1_history.py
@@ -0,0 +1,7 @@
+"""
+August 29, 2022.
+We had a wheel (TW3428) with a slightly low OX-1. I want to know if this low OX is statistically out of bounds.
+For example, in the three sigma rule (68-95-99.7), there should be only 0.3% outside of 3 sigma. Where is my low OX-1,
+and is it outside of 3-sigma? 
+
+"""
